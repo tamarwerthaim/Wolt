d865fcfd911993594253fd88b83dccb7d31b9c55
tamar werthaim <tamarwert@gmail.com>
2026-04-27 19:32:28 +0300

    task 15 done

---
+++ User.h
@@ -0,0 +1,20 @@
+#ifndef USER_H
+#define USERE_H
+
+#include "Product.h"
+#include <vector>
+
+class User{
+    private:
+        int userID;
+        std::vector<Product> productList;
+
+    public:
+        User(int id)
+        void addProduct(const Product& p)
+        const std::vector<Product>& getProducts() const
+        int getID() const
+}
+
+#endif
+
