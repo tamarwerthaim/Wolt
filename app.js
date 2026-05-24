f1ff26a6ea024f0743fdca0f41a5450d95b25435
Mmoriya2736 <moriyamalkiel123@gmail.com>
2026-05-24 12:57:47 +0300

    create userRoutes

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ app.use(express.json());
 
 // import routes
 // const restaurantRouter = require('./routes/restaurantRouter');
-// const userRouter = require('./routes/userRouter');
+const userRouter = require('./routes/userRouter');
+//const tokenRouter = require('./routes/tokenRouter');
 // const productRouter = require('./routes/productRouter');
 // const orderRouter = require('./routes/orderRouter');
 // const searchRouter = require('./routes/searchRouter');
 
 // connect URL paths to routers
 // app.use('/api/restaurants', restaurantRouter);
-// app.use('/api/users', userRouter);
+app.use('/api/users', userRouter);
+//app.use('/api/tokens', tokenRouter);
 // app.use('/api/products', productRouter);
 // app.use('/api/orders', orderRouter);
 // app.use('/api/search', searchRouter);
