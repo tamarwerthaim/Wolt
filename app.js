6e1da51c1783d8c22535283abe6643f9abaa6887
tamar werthaim <tamarwert@gmail.com>
2026-05-24 12:07:31 +0300

    create main and app

---
+++ app.js
@@ -0,0 +1,28 @@
+// import express and create an instance of it. app will be our server.
+const express = require('express');
+const app = express();
+
+// app will use express.json() middleware to parse JSON request bodies
+app.use(express.json());
+
+// import routes
+// const restaurantRouter = require('./routes/restaurantRouter');
+// const userRouter = require('./routes/userRouter');
+// const productRouter = require('./routes/productRouter');
+// const orderRouter = require('./routes/orderRouter');
+// const searchRouter = require('./routes/searchRouter');
+
+// connect URL paths to routers
+// app.use('/api/restaurants', restaurantRouter);
+// app.use('/api/users', userRouter);
+// app.use('/api/products', productRouter);
+// app.use('/api/orders', orderRouter);
+// app.use('/api/search', searchRouter);
+
+// TODO- delete
+app.get('/ping', (req, res) => {
+    res.status(200).send('pong! השרת של תמר עובד בהצלחה!');
+});
+
+// ready for delivery
+module.exports = app;
---
+++ main.js
@@ -0,0 +1,16 @@
+// import app.js
+const app = require('./app'); 
+
+const PORT = 3000;
+
+// TODO- delete 
+// start the server
+app.listen(PORT, () => {
+    console.log(`=============================================`);
+    console.log(`🚀 WebServer is running on http://localhost:${PORT}`);
+    console.log(`=============================================`);
+});
+
+// TODO- add
+// start the server
+//app.listen(PORT);
