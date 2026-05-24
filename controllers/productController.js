c5f20a97fe34ab5cd642d2f64806df0a38ad0811
Mmoriya2736 <moriyamalkiel123@gmail.com>
2026-05-24 20:09:38 +0300

    edit readme and delete servises

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 import ProductModel from '../models/productModel.js';
 import * as userModel from '../models/userModel.js'
-import { sendToCpp } from '../services/socket.js';
+import { sendToCpp } from '../socket.js';
 
 class ProductController {
     //pull all products from a restaurant's menu
