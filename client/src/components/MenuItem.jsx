30c8bf0cdb536126f90ca322451c3297e4d54cfb
tamar werthaim <tamarwert@gmail.com>
2026-06-14 15:08:10 +0300

    change image in edit

--- client/src/components/MenuItem.jsx
+++ client/src/components/MenuItem.jsx
@@ -127,7 +127,10 @@ const MenuItem = ({ product, cart, onAdd, onRemove, restaurantId, restaurantName
                         }}
                         title="Edit Product"
                     >
-                        ✏️
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="var(--wolt-dark)" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="edit-pencil-svg">
+                            <path d="M12 20h9"></path>
+                            <path d="M16.5 3.5a2.12 2.12 0 0 1 3 3L7 19l-4 1 1-4Z"></path>
+                        </svg>
                     </button>
                 )}
 
