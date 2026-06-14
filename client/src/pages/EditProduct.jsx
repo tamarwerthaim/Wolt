f68438390efc45e365f79fefec9aabba590b1826
roni <roni.laufer@live.biu.ac.il>
2026-06-14 10:28:30 +0300

    order folders2

--- client/src/pages/EditProduct.jsx
+++ client/src/pages/EditProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import './LoginRegisterStyles.css';
+import './FormStyles.css';
 
 const EditProduct = () => {
     const { id: restaurantId, pld: productId } = useParams();
