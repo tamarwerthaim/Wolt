7dda99f52192a550bf07f019b787830f90486dcf
roni <roni.laufer@live.biu.ac.il>
2026-06-24 16:48:23 +0300

    fix config

--- mobile/config.js
+++ mobile/config.js
@@ -1,7 +1,17 @@
-import { Platform } from 'react-native';
+import { Platform, NativeModules } from 'react-native';
 
-// Configuration file for the mobile client
-// Using the local IP address of the machine to allow the Expo Go app on physical devices to connect.
-export const API_BASE_URL = 'http://172.20.10.2:3000';
+// Dynamically retrieve the IP address of the machine running the Metro bundler.
+// This allows different developers to run the app without changing the IP address manually.
+let packagerIp = '192.168.1.125'; // Fallback default IP
+
+if (__DEV__) {
+  const scriptURL = NativeModules.SourceCode?.scriptURL || '';
+  const ipAddressMatch = scriptURL.match(/:\/\/([^\/:]+)/);
+  if (ipAddressMatch && ipAddressMatch[1]) {
+    packagerIp = ipAddressMatch[1];
+  }
+}
+
+export const API_BASE_URL = `http://${packagerIp}:3000`;
 
 export const ROUNDED_FONT = Platform.OS === 'ios' ? 'ui-rounded' : 'sans-serif-condensed';
